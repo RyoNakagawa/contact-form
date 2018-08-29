b9a3ba6356c15ed88c69940e5c29be0db554c5d3
RyoNakagawa <fkrowone@gmail.com>
2018-08-29 20:48:59 +0900

    Revert "READMEを更新する"

--- db/migrate/20180825024639_create_inquiry_kinds.rb
+++
@@ -1,9 +0,0 @@
-class CreateInquiryKinds < ActiveRecord::Migration
-  def change
-    create_table :inquiry_kinds do |t|
-      t.string :name, null: false
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-inquiry_kinds = [
-  {id: 1, name: '製品について'},
-  {id: 2, name: '注文について'},
-  {id: 3, name: '発送について'},
-  {id: 4, name: '返品について'},
-  {id: 5, name: 'その他'},
-]
-
-inquiry_kinds.each do |inquiry_kind|
-  InquiryKind.find_or_create_by(inquiry_kind)
-end
